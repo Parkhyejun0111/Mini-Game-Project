2b92cb6031383a801473d6b14d47249f6d6343f7
Hyejun <qkzpwns0111@gmail.com>
2026-05-20 15:40:10 +0900

    메인 메뉴창 및 게임 연결 구현

--- app.py
+++ app.py
@@ -1,29 +1,43 @@
-from crocodile_game.crocodile_dentist_game import CrocodileDentistGame
-from blackjack.blackjack_game import BlackJackGame
+# app.py
+
+from crocodile_game.crocodile_dentist_game import Crocodile
+from blackjack.blackjack import BlackjackGame
 
 
 class App:
 
+    def __init__(self):
+
+        # 악어게임 객체
+        self.crocodile = Crocodile()
+
+        # 블랙잭 객체
+        self.blackjack = BlackjackGame(
+            player_name="USER",
+            starting_chips=100
+        )
+
     def print_menu(self):
 
         print()
-        print("🎪 DEVIL CASINO 🎪")
-        print("1. 악어이빨게임")
+        print("=================================")
+        print("🎮 MINI GAME MENU 🎮")
+        print("=================================")
+        print("1. 악어 이빨 게임")
         print("2. 블랙잭")
         print("3. 게임 종료")
-
+        print("=================================")
 
     def input_menu(self):
 
         while True:
 
             try:
-                menu = int(input("메뉴 선택 : "))
+                menu = int(input("메뉴를 선택하세요: "))
                 return menu
 
             except ValueError:
                 print("숫자만 입력해주세요.")
-
 
     def run(self):
 
@@ -33,20 +47,25 @@ class App:
 
             menu = self.input_menu()
 
+            # 악어게임
             if menu == 1:
 
-                game = CrocodileDentistGame()
-                game.play()
+                print("\n🐊 악어 게임 시작!\n")
 
+                self.crocodile.crocodile_game()
+
+            # 블랙잭
             elif menu == 2:
 
-                game = BlackJackGame()
-                game.play()
+                print("\n🃏 블랙잭 시작!\n")
 
+                self.blackjack.play()
+
+            # 종료
             elif menu == 3:
 
-                print("게임 종료")
+                print("\n게임을 종료합니다.")
                 break
 
             else:
-                print("다시 입력해주세요.")
+                print("메뉴를 다시 선택해주세요.")
--- crocodile_game/crocodile_dentist_game.py
+++ crocodile_game/crocodile_dentist_game.py
@@ -5,122 +5,122 @@ import sys
 
 from config import USER_ID, USER_PW, history
 
+class Crocodile : 
+    """ def login():
+        for i in range(3):
+            user_id = input("Enter ID: ")
+            user_pw = input("Enter PASSWORD: ")
 
-def login():
-    for i in range(3):
-        user_id = input("Enter ID: ")
-        user_pw = input("Enter PASSWORD: ")
+            if user_id == USER_ID and user_pw == USER_PW:
+                print(f"\n{user_id} logged in.\n")
+                return True
+            else:
+                print(f"Unable to log in ({i+1}/3)")
 
-        if user_id == USER_ID and user_pw == USER_PW:
-            print(f"\n{user_id} logged in.\n")
-            return True
+        print("Access denied.")
+        sys.exit() """
+
+
+    def crocodile_game():
+
+        teeth = list(range(1, 21))
+        bomb = random.choice(teeth)
+        used_teeth = []
+        turn = "USER"
+
+        while True:
+
+            print("\n====================")
+            print("Teeth left:")
+
+            for tooth in teeth:
+                if tooth not in used_teeth:
+                    print(tooth, end=" ")
+
+            print("\n====================")
+
+            # USER TURN
+            if turn == "USER":
+
+                try:
+                    choice = int(input("\nUSER turn - Pick a tooth: "))
+                except ValueError:
+                    print("Enter a number.")
+                    continue
+
+                if choice < 1 or choice > 20:
+                    print("It should be number of 1 ~ 20.")
+                    continue
+
+                if choice in used_teeth:
+                    print("Already taken.")
+                    continue
+
+                used_teeth.append(choice)
+
+                if choice == bomb:
+                    print("\nSnap!!")
+                    print("Crocodile bit YOU!!")
+                    history.append("COMPUTER WIN")
+                    break
+
+                turn = "COMPUTER"
+
+            # COMPUTER TURN
+            else:
+
+                possible_choices = [
+                    tooth for tooth in teeth if tooth not in used_teeth
+                ]
+
+                computer_choice = random.choice(possible_choices)
+
+                print(f"\nCOMPUTER chose {computer_choice} !")
+
+                used_teeth.append(computer_choice)
+
+                if computer_choice == bomb:
+                    print("\nSnap!!")
+                    print("Crocodile bit COMPUTER!!")
+                    history.append("USER WIN")
+                    break
+
+                turn = "USER"
+
+""" 
+    def game_history():
+
+        print("\n===== GAME HISTORY =====")
+
+        if len(history) == 0:
+            print("None")
         else:
-            print(f"Unable to log in ({i+1}/3)")
-
-    print("Access denied.")
-    sys.exit()
+            for result in history:
+                print("-", result)
 
 
-def crocodile_game():
+    def menu():
 
-    teeth = list(range(1, 21))
-    bomb = random.choice(teeth)
-    used_teeth = []
-    turn = "USER"
+        while True:
 
-    while True:
+            print("\n1. 게임 시작")
+            print("2. 기록 보기")
+            print("3. 게임 종료")
 
-        print("\n====================")
-        print("Teeth left:")
+            choice = input("메뉴를 선택하세요: ")
 
-        for tooth in teeth:
-            if tooth not in used_teeth:
-                print(tooth, end=" ")
-
-        print("\n====================")
-
-        # USER TURN
-        if turn == "USER":
-
-            try:
-                choice = int(input("\nUSER turn - Pick a tooth: "))
-            except ValueError:
-                print("Enter a number.")
-                continue
-
-            if choice < 1 or choice > 20:
-                print("It should be number of 1 ~ 20.")
-                continue
-
-            if choice in used_teeth:
-                print("Already taken.")
-                continue
-
-            used_teeth.append(choice)
-
-            if choice == bomb:
-                print("\nSnap!!")
-                print("Crocodile bit YOU!!")
-                history.append("COMPUTER WIN")
+            if choice == "1":
+                crocodile_game()
+            elif choice == "2":
+                game_history()
+            elif choice == "3":
+                print("게임 종료합니다.")
                 break
-
-            turn = "COMPUTER"
-
-        # COMPUTER TURN
-        else:
-
-            possible_choices = [
-                tooth for tooth in teeth if tooth not in used_teeth
-            ]
-
-            computer_choice = random.choice(possible_choices)
-
-            print(f"\nCOMPUTER chose {computer_choice} !")
-
-            used_teeth.append(computer_choice)
-
-            if computer_choice == bomb:
-                print("\nSnap!!")
-                print("Crocodile bit COMPUTER!!")
-                history.append("USER WIN")
-                break
-
-            turn = "USER"
-
-
-def game_history():
-
-    print("\n===== GAME HISTORY =====")
-
-    if len(history) == 0:
-        print("None")
-    else:
-        for result in history:
-            print("-", result)
-
-
-def menu():
-
-    while True:
-
-        print("\n1. 게임 시작")
-        print("2. 기록 보기")
-        print("3. 게임 종료")
-
-        choice = input("메뉴를 선택하세요: ")
-
-        if choice == "1":
-            crocodile_game()
-        elif choice == "2":
-            game_history()
-        elif choice == "3":
-            print("게임 종료합니다.")
-            break
-        else:
-            print("메뉴를 다시 선택하세요.")
+            else:
+                print("메뉴를 다시 선택하세요.")
 
 
 
-if __name__ == "__main__":
-    login()
-    menu()
+    if __name__ == "__main__":
+        login()
+        menu() """
